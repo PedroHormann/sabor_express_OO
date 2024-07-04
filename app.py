9c938e0f8926f38b1e412ad6b940f1a115e40b91
Pedro Dutra <135768773+PedroHormann@users.noreply.github.com>
2024-07-04 10:44:10 -0300

    Finalizando app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from modelos.restaurante import Restaurante
 
-restaurante_praca = Restaurante('praça','gourmet')
-restaurante_mexican = Restaurante('mexican food','mexicana')
-restaurante_japa = Restaurante('japapei','japonesa')
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Laís', 8)
+restaurante_praca.receber_avaliacao('Emy', 2)
 
+restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+restaurante_mexicano.receber_avaliacao('Ana', 9)
+restaurante_mexicano.receber_avaliacao('Carlos', 7)
 
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+restaurante_japones.receber_avaliacao('Pedro', 8)
+restaurante_japones.receber_avaliacao('Maria', 6)
+restaurante_japones.receber_avaliacao('João', 7)
 
 def main():
     Restaurante.listar_restaurantes()
